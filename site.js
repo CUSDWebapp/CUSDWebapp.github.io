618333642eeff1c09edec241a121418e085fcda4
Ryan Bohannon <ryanbrohannon@gmail.com>
2015-11-11 15:28:46 -0700

    Interactive ugly tables

--- site.js
+++ site.js
@@ -93,3 +93,7 @@ $(document).ready(function (){
 
 });
 
+$("a[id^=show_]").click(function(event) {
+      $("#extra_" + $(this).attr('id').substr(5)).slideToggle("slow");
+          event.preventDefault();
+})
